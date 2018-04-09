2ab09f1bff156f628fc3bfac6eccc5a9d4cdf39e
nicewahson <nicelj92@gmail.com>
2018-04-09 13:51:11 +0800

    z

--- code/2.js
+++ code/2.js
@@ -1,1 +1,11 @@
-console.log(process.argv.slice(2));
+// console.log(process.argv.slice(2));
+function nodeToArr(node){
+    var arr = [];
+    node.val && arr.push(node.val);
+    if(node.next){
+        arr = arr.concat(nodeToArr(node.next))
+    }
+    return arr;
+}
+
+console.log(nodeToArr({val:2,next:{val:4,next:null}}))
--- source/_posts/jquery的理解.md
+++ source/_posts/jquery的理解.md
@@ -4,3 +4,4 @@ date: 2018-03-21 17:42:39
 tags:
 ---
  ![jquery](jquerybase.png)
+ 关于jq
